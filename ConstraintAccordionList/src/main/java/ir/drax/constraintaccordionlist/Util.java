5345e0a5bedd4d291b314c55ae4df18621c39dd6
Siamak <ssiasoft@gmail.com>
2019-01-06 23:09:44 +0330

    -Ability to build list multiple times and push items from every where

---
+++ ConstraintAccordionList/src/main/java/ir/drax/constraintaccordionlist/Util.java
@@ -0,0 +1,13 @@
+package ir.drax.constraintaccordionlist;
+
+import java.util.Random;
+
+/**
+ * Created by Drax on 1/4/19.
+ */
+
+public class Util {
+    public static int genRandId(){
+        return new Random().nextInt(100000);
+    }
+}
